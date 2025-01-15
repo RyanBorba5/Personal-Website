d0367e942e1ce46c40d115da5e3e2fd7173bfefb
RyanBorba5 <ryanborba64@gmail.com>
2025-01-14 20:17:40 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -73,10 +73,26 @@ function updateJavaProgressText() {
     progressText.textContent = `${percentage.toFixed(0)}%`; // Update the text content of the paragraph with percentage 
 }
 
+function updatePythonProgressText() {
+    const progressBar = document.querySelector('#python-progress');
+    const barBounds = document.querySelector('.bar');
+    const progressText = document.querySelector('#python-progress-text p');
+    const progressBarStyle = window.getComputedStyle(progressBar);
+    const barBoundsStyle = window.getComputedStyle(barBounds);
+
+    const maxWidth = parseFloat(barBoundsStyle.getPropertyValue('width')); // Get the computed width of the bar in pixels
+    const width = parseFloat(progressBarStyle.getPropertyValue('width')); // Get the current width of the progress bar in pixels
+
+    const percentage = (width / maxWidth) * 100; // Calculate the percentage width of the progress bar
+
+    progressText.textContent = `${percentage.toFixed(0)}%`; // Update the text content of the paragraph with percentage 
+}
+
 // Example of setting an interval to update the progress text every second
 setInterval(updateCSharpProgressText, 500);
 setInterval(updateJavascriptProgressText, 500);
 setInterval(updateHTMLProgressText, 500);
 setInterval(updateCSSProgressText, 500);
 setInterval(updateJavaProgressText, 500);
+setInterval(updatePythonProgressText, 500);
 
